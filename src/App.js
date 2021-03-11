15db8b26d21a025ff709a6ed040b60011039cb87
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-03-11 03:05:14 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import M, { Modal } from 'materialize-css'
+import weather from './weather.json'
+
+import Header from './Header'
+import Pop from './Pop'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      temp: '',
+      cityName: '',
+      weather: '',
+      high: '',
+      low: '',
+      icon: '',
+      showModal: false,
+    }
+  }
+
+  componentDidMount() {
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    // axios.get(url).then((response) => {
+    //   this.setState({
+    //     temp: response.data.main.temp,
+    //   })
+    // })
+
+    this.setState({
+      temp: weather[0].main.temp,
+      cityName: weather[0].name,
+      weather: weather[0].weather[0].description,
+      high: weather[0].main.temp_max,
+      low: weather[0].main.temp_min,
+      icon: weather[0].weather[0].icon,
+    })
+
+    var elems = document.querySelectorAll('.modal')
+    var instances = M.Modal.init(elems)
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Header cityName={this.state.cityName} temp={this.state.temp} />
+
+      <button data-target='modal1' className='btn modal-trigger'>
+        Modal
+       </button>
+
+        <Pop cityName={this.state.cityName} high={this.state.high} low={this.state.low} weather={this.state.weather} icon={this.state.iconUrl} />
+
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = ({cityName, temp}) => {
+    return (
+        <div className='row'>
+          <h2>{cityName}</h2>
+          <h5>Today's Temperature: {temp}</h5>
+        </div>
+    )
+}
+
+// const Header = ({cityName, temp}) => {
+//     return (
+//         <div className='row'>
+//           <h2>{cityName}</h2>
+//           <h5>Today's Temperature: {temp}</h5>
+//         </div>
+//     )
+// }
+
+export default Header
